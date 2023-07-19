e13a0a1844c6ac3af2cf31256f59daa943f6f10f
GSC Bravo <gscbravo@gmail.com>
2023-07-19 14:58:10 -0500

    quotes

--- src/app.py
+++ src/app.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # show media attachments
-    media = '1' if request.args.get('media', '') == '1' else ''
+    media = "1" if request.args.get("media", "") == "1" else ""
 
     # fediverse server
-    url = 'https://mastodon.social'
+    url = "https://mastodon.social"
 
     # get posts
-    response = requests.get(f'{url}/api/v1/timelines/public?limit=40')
+    response = requests.get(f"{url}/api/v1/timelines/public?limit=40")
     posts = response.json()
 
-    return render_template('index.html', posts=posts, media=media)
+    return render_template("index.html", posts=posts, media=media)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
 
